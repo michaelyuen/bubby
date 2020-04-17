f9280ef8bffb28845a71eb7ce59f02ca7529bca8
Michael Yuen <michaelclaytonyuen@gmail.com>
2020-04-16 20:01:18 -0400

    mm gross (#13)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -49,18 +49,18 @@ const wsLink = new WebSocketLink({
 
 // using the ability to split links, you can send data to each link
 // depending on what kind of operation is being sent
-// const link = split(
-//   // split based on operation type
-//   ({ query }) => {
-//     const definition = getMainDefinition(query);
-//     return (
-//       definition.kind === "OperationDefinition" &&
-//       definition.operation === "subscription"
-//     );
-//   },
-//   wsLink,
-//   httpLink
-// );
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
   link: ApolloLink.from([
