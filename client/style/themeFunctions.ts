5f6b1dd8f284e94fcaf8878804c4b0ad4277d949
sue <sueann.ioanis@gmail.com>
2020-10-04 02:25:03 -0400

    Fix build

--- client/style/themeFunctions.ts
+++ client/style/themeFunctions.ts
@@ -1,4 +1,4 @@
-export type any = {
+export type Theme = {
   color: {
     background: String;
     buttonText: String;
@@ -23,18 +23,25 @@ export type any = {
   };
 };
 
-export const colorBackground = (props: any) => props.color.background;
-export const colorButtonText = (props: any) => props.color.buttonText;
-export const colorSecondary = (props: any) => props.color.secondary;
-export const colorLink = (props: any) => props.color.link;
-export const colorText = (props: any) => props.color.text;
-export const fontMedium = (props: any) => props.font.medium;
-export const fontSmall = (props: any) => props.font.small;
+export const colorBackground = (props: any) =>
+  props.color && props.color.background;
+export const colorButtonText = (props: any) =>
+  props.color && props.color.buttonText;
+export const colorSecondary = (props: any) =>
+  props.color && props.color.secondary;
+export const colorLink = (props: any) => props.color && props.color.link;
+export const colorText = (props: any) => props.color && props.color.text;
+export const fontMedium = (props: any) => props.font && props.font.medium;
+export const fontSmall = (props: any) => props.font && props.font.small;
 export const gridFullPageFormWidth = (props: any) =>
-  props.grid.fullPageFormWidth;
-export const gridWidth = (props: any) => props.grid.width;
-export const gridMaxWidth = (props: any) => props.grid.maxWidth;
-export const spacingBorder = (props: any) => props.spacing.border;
-export const spacingBorderRadius = (props: any) => props.spacing.borderRadius;
-export const spacingDefault = (props: any) => props.spacing.default;
-export const themeSpacingLarge = (props: any) => props.spacing.large;
+  props.grid && props.grid.fullPageFormWidth;
+export const gridWidth = (props: any) => props.grid && props.grid.width;
+export const gridMaxWidth = (props: any) => props.grid && props.grid.maxWidth;
+export const spacingBorder = (props: any) =>
+  props.spacing && props.spacing.border;
+export const spacingBorderRadius = (props: any) =>
+  props.spacing && props.spacing.borderRadius;
+export const spacingDefault = (props: any) =>
+  props.spacing && props.spacing.default;
+export const themeSpacingLarge = (props: any) =>
+  props.spacing && props.spacing.large;
